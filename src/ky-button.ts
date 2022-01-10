24a9244459b5496b1202fab2c0c96d8d255eafcb
kimizuy <kimizuyamasaki@gmail.com>
2022-01-10 16:18:52 +0900

    initial commit

---
+++ src/ky-button.ts
@@ -0,0 +1,17 @@
+import { html, css, LitElement } from 'lit'
+import { customElement } from 'lit/decorators.js'
+
+@customElement('ky-button')
+export class KyButton extends LitElement {
+  static override styles = css`
+    button {
+      color: blue;
+    }
+  `
+
+  override render() {
+    return html`<button>
+      <slot><span>fallback text</span></slot>
+    </button>`
+  }
+}
